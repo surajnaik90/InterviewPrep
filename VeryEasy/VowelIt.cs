b44d299cd4017f0453fb63cd207b59cfd3b655b9
Suraj Naik <suraj.b.naik@live.com>
2022-06-22 16:40:00 +0530

    vowel it

---
+++ VeryEasy/VowelIt.cs
@@ -0,0 +1,19 @@
+﻿public static class VowelIt
+{
+    public static string solve(string A)
+    {
+        if(string.IsNullOrEmpty(A)) { return string.Empty; }
+
+        string output = string.Empty;
+        char[] vowels = new char[] {'a','e','i','o','u'};
+
+        for (int i = 0; i < A.Length; i++)
+        {
+            if (vowels.Contains(A[i])) {
+                output = string.Concat(output, A[i]);
+            }
+        }
+
+        return output;
+    }
+}
