4ba07fb630e90793f6248685e1df079444448008
Suraj Naik <suraj.b.naik@live.com>
2022-06-25 16:10:15 +0530

    slightly optmised

--- Medium/LongestPalindrome.cs
+++ Medium/LongestPalindrome.cs
@@ -50,6 +50,45 @@ public static class LongestPalindrome
         return output;
     }
 
+
+    //Slightly better version. But still time limit exceeds. Avoid for loops as much as possible.
+    //The below operation is better than operation1. THis operation executed 80 more test cases (total 125)
+    //as compared to 45 testcases on leetcode.
+    public static string Operation2(string str)
+    {
+        if (str.Length == 0) { return string.Empty; }
+
+        string output = string.Empty; string substr = string.Empty;
+        List<string> possiblePalindromes = new List<string>();
+        int count = 0;
+
+        for (int i = 0; i < str.Length; i++)
+        {
+            for (int j = i; j < str.Length; j++)
+            {
+                substr = str.Substring(i, j - i + 1);
+
+                if (i == j)
+                {
+                    substr = string.Concat("", str[i]);
+                }
+
+                if ((str[i] == str[j]) && (!(possiblePalindromes.Contains(substr))))
+                {
+                    if (IsPalindrome(substr))
+                    {
+                        if (count < substr.Length)
+                        {
+                            output = substr;
+                            count = substr.Length;
+                        }
+                    }
+                }
+            }
+        }
+        return output;
+    }
+
     private static bool IsPalindrome(string str)
     {
         if(str.Length == 1) { return true; }
