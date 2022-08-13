5962f366f60378690528c12ebe16cb25930a4c5e
Suraj Naik <suraj.b.naik@live.com>
2022-08-13 10:50:58 +0530

    Sum of digits

---
+++ DSAAssignments/Recursion/SumOfDigits.cs
@@ -0,0 +1,49 @@
+﻿/*
+Given a number A, we need to find the sum of its digits using recursion.
+
+Problem Constraints
+1 <= A <= 109
+
+Input Format
+The first and only argument is an integer A.
+
+Output Format
+Return an integer denoting the sum of digits of the number A.
+
+Example Input
+Input 1:
+
+ A = 46
+Input 2:
+
+ A = 11
+
+Example Output
+Output 1:
+
+ 10
+Output 2:
+
+ 2
+
+Example Explanation
+Explanation 1:
+
+ Sum of digits of 46 = 4 + 6 = 10
+Explanation 2:
+
+ Sum of digits of 11 = 1 + 1 = 2
+ */
+public static class SumOfDigits
+{
+    public static int solve(int A)
+    {
+        return find(A);
+    }
+    private static int find(int n)
+    {
+        if(n<1) { return 0; }
+
+        return find(n / 10) + (n%10);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -457,10 +457,12 @@ input.Add(input1); input.Add(input2); input.Add(input3);
 
 //int res = Fibonacci.solve(9);
 
-PrintReverseString.solve("visualstudio");
+//PrintReverseString.solve("visualstudio");
 
 //int res = LengthofLongestConsecutiveOnes.solve("111011100001011111111110000");
 
 //int res = CheckPalindrome.solve("naman");
+
+int ans = SumOfDigits.solve(4568656);
 
 Console.WriteLine("done");
