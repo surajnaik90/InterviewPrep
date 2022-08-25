0e796285d927daf0888d0b6dc934899c93bdc3ec
Suraj Naik <suraj.b.naik@live.com>
2022-08-25 15:32:11 +0530

    Inorder traversal

---
+++ DSAAssignments/Trees/Inordertraversal.cs
@@ -0,0 +1,126 @@
+﻿/*
+Given a binary tree, return the inorder traversal of its nodes' values.
+
+NOTE: Using recursion is not allowed.
+
+Problem Constraints
+1 <= number of nodes <= 105
+
+Input Format
+First and only argument is root node of the binary tree, A.
+
+Output Format
+Return an integer array denoting the inorder traversal of the given binary tree.
+
+Example Input
+Input 1:
+
+   1
+    \
+     2
+    /
+   3
+Input 2:
+
+   1
+  / \
+ 6   2
+    /
+   3
+
+
+Example Output
+Output 1:
+
+ [1, 3, 2]
+Output 2:
+
+ [6, 1, 3, 2]
+
+
+Example Explanation
+Explanation 1:
+
+ The Inorder Traversal of the given tree is [1, 3, 2].
+Explanation 2:
+
+ The Inorder Traversal of the given tree is [6, 1, 3, 2].
+ */
+public static class InorderTraversal
+{
+    //Created a custom stack & have not used the language provided stack
+    public static List<int> solve(TreeNode A)
+    {
+        List<int> output = new List<int>();
+
+        if (A == null) { return output; }
+
+        //Declare a stack
+        Dictionary<TreeNode, char> stack = new Dictionary<TreeNode, char>();
+        stack.Add(A, 'l'); 
+
+        int i = 0; TreeNode node; char traverseType;
+        while (i >= 0)
+        {
+
+            //Extract the node & traversetype details
+            node = stack.ElementAt(i).Key;
+            traverseType = stack.ElementAt(i).Value;
+
+            switch (traverseType)
+            {
+
+                case 'l':
+                    if (node.left == null || node.left.val == -1)
+                    {
+                        output.Add(node.val);
+                        stack[node] = 'r';
+                        continue;
+                    }
+                    else
+                    {
+                        stack.Add(node.left, 'l');
+                        i++;
+                    }
+                    break;
+
+                case 'r':
+                    if (node.right == null || node.right.val == -1)
+                    {
+                        stack[node] = 'd';
+                        continue;
+                    }
+                    else
+                    {
+                        stack.Add(node.right, 'l');
+                        i++;
+                    }
+                    break;
+
+                case 'd':
+                    if (stack.Count == 1)
+                    {
+                        return output;
+                    }
+                    stack.Remove(node); i--;
+
+                    node = stack.ElementAt(i).Key;
+                    traverseType = stack.ElementAt(i).Value;
+
+                    if (traverseType == 'l')
+                    {
+                        output.Add(node.val);
+                        stack[node] = 'r';
+                    }
+                    else if (traverseType == 'r')
+                    {
+                        stack[node] = 'd';
+                    }
+                    break;
+            }
+
+        }
+
+        return output;
+    }
+}
