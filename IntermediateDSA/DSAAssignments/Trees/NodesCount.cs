ba1cbcc0995d69026dbcc252e8997822afe9f5e4
Suraj Naik <suraj.b.naik@live.com>
2022-08-27 17:21:22 +0530

    Nodes count

--- IntermediateDSA/DSAAssignments/Trees/NodesCount.cs
+++ IntermediateDSA/DSAAssignments/Trees/NodesCount.cs
@@ -46,67 +46,27 @@ Clearly, there are 4 nodes 1, 4, 3 and 2.
  */
 public static class NodesCount
 {
-    //Created a custom stack & have not used the language provided stack
     public static int solve(TreeNode A)
     {
+        if(A==null) { return 0; }
+
         int output = 0;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.Push(A); output++;
 
-        if (A == null) { return output; }
+        while(stack.Count != 0) {
 
-        //Declare a stack
-        Dictionary<TreeNode, char> stack = new Dictionary<TreeNode, char>();
-        stack.Add(A, 'l');output++;
+            TreeNode node = stack.Pop();
 
-        int i = 0; TreeNode node; char traverseType;
-        
-        while (i >= 0) {
-
-            //Extract the node & traversetype details
-            node = stack.ElementAt(i).Key;
-            traverseType = stack.ElementAt(i).Value;
-
-            switch (traverseType) {
-
-                case 'l':
-                    if (node.left == null || node.left.val == -1) {
-                        stack[node] = 'r';
-                        continue;
-                    }
-                    else {
-                        stack.Add(node.left, 'l');                         
-                        i++; output++;
-                    }
-                    break;
-
-                case 'r':
-                    if (node.right == null || node.right.val == -1) {
-                        stack[node] = 'd';
-                        continue;
-                    }
-                    else {
-                        stack.Add(node.right, 'l');
-                        i++; output++;
-                    }
-                    break;
-
-                case 'd':
-                    if (stack.Count == 1) {
-                        return output;
-                    }
-                    stack.Remove(node); i--;
-
-                    node = stack.ElementAt(i).Key;
-                    traverseType = stack.ElementAt(i).Value;
-
-                    if (traverseType == 'l') {
-                        stack[node] = 'r';
-                    }
-                    else if (traverseType == 'r') {
-                        stack[node] = 'd';
-                    }
-                    break;
+            if (node.left != null) {
+                stack.Push(node.left);
+                output++;
             }
-
+             
+            if (node.right != null) {
+                stack.Push(node.right);
+                output++;
+            }
         }
 
         return output;
