ae485111ed43a53ff91ef7936891c02cfb346082
Suraj Naik <suraj.b.naik@live.com>
2022-10-09 09:27:03 +0530

    Consecutive elements

--- AdvancedDSA/Sorting/ConsecutiveElements.cs
+++ AdvancedDSA/Sorting/ConsecutiveElements.cs
@@ -41,21 +41,9 @@ public static class ConsecutiveElements
 {
     public static int solve(List<int> A)
     {
+        A = QuickSort.solve(A);
+
         int N = A.Count;
-
-        for (int i = 0; i < N-1; i++) {
-
-            for (int j = i+1; j < N; j++) {
-
-                if (A[i] > A[j]) {
-
-                    int temp = A[i];
-                    A[i] = A[j];
-                    A[j] = temp;
-                }
-            }
-        }
-
         for (int i = 0; i < N-1; i++) {
 
             if ((A[i+1] - A[i]) != 1) {
