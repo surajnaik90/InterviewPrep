7a42222eb9557195781611961a319d13ff670243
Suraj Naik <suraj.b.naik@live.com>
2022-11-27 18:34:15 +0530

    Top view

--- AdvancedDSA/Trees/TopView.cs
+++ AdvancedDSA/Trees/TopView.cs
@@ -55,40 +55,104 @@ Top view is described.
  */
 public static class TopView
 {
+    public class VerticalNode
+    {
+        public TreeNode treeNode;
+        public int verticalAxis;
+        public VerticalNode(TreeNode node, int axis)
+        {
+            this.treeNode = node;
+            this.verticalAxis = axis;
+        }
+    }
     public static List<int> solve(TreeNode A)
     {
         List<int> res = new List<int>();
+        int min = 0, max = 0;
 
-        Queue<TreeNode> q = new Queue<TreeNode>();
-        q.Enqueue(A); q.Enqueue(null);
+        Queue<VerticalNode> q = new Queue<VerticalNode>();
+        Queue<VerticalNode> sq = new Queue<VerticalNode>();
+        Dictionary<int, List<int>> axisNodes = new Dictionary<int, List<int>>();
+
+        q.Enqueue(new VerticalNode(A, 0)); q.Enqueue(null);
+        sq.Enqueue(new VerticalNode(A, 0)); sq.Enqueue(null);
 
 
-        List<List<int>> levelNodes = new List<List<int>>();
-        List<int> list = new List<int>();
+        axisNodes.Add(0, new List<int>() { A.val });
 
-        while (q.Count > 0) {
+        while (q.Count > 0)
+        {
 
-            TreeNode node = q.Dequeue();
+            VerticalNode verticalNode = q.Dequeue();
 
-            if (node == null && q.Count == 0) { break; }
+            if (verticalNode == null && q.Count == 0)
+            {
+                break;
+            }
 
-            if (node == null) {
+            if (verticalNode == null)
+            {
                 q.Enqueue(null);
-                levelNodes.Add(list);
-                list = new List<int>();
+                sq.Enqueue(null);
                 continue;
             }
 
-            if (node.left != null) {
-                q.Enqueue(node.left);
-                list.Add(node.left.val);
+            int axis = verticalNode.verticalAxis;
+
+            if (verticalNode.treeNode.left != null)
+            {
+
+                int leftAxis = axis - 1;
+
+                q.Enqueue(new VerticalNode(verticalNode.treeNode.left,
+                    leftAxis));
+                sq.Enqueue(new VerticalNode(verticalNode.treeNode.left,
+                    leftAxis));
+
+                if (axisNodes.ContainsKey(leftAxis))
+                {
+                    axisNodes[leftAxis].Add(verticalNode.treeNode.left.val);
+                }
+                else
+                {
+                    axisNodes.Add(leftAxis,
+                        new List<int>() { verticalNode.treeNode.left.val });
+                }
+
+                min = Math.Min(min, leftAxis);
             }
 
-            if (node.right != null) {
-                q.Enqueue(node.right);
-                list.Add(node.right.val);
+            if (verticalNode.treeNode.right != null)
+            {
+
+                int rightAxis = axis + 1;
+
+                q.Enqueue(new VerticalNode(verticalNode.treeNode.right,
+                    rightAxis));
+                sq.Enqueue(new VerticalNode(verticalNode.treeNode.right,
+                    rightAxis));
+
+                if (axisNodes.ContainsKey(rightAxis))
+                {
+                    axisNodes[rightAxis].Add(verticalNode.treeNode.right.val);
+                }
+                else
+                {
+                    axisNodes.Add(rightAxis,
+                        new List<int>() { verticalNode.treeNode.right.val });
+                }
+
+                max = Math.Max(max, rightAxis);
             }
         }
+
+        for (int i = min; i <= max; i++) {
+
+            List<int> verticalNodes = axisNodes[i];
+
+            res.Add(verticalNodes[0]);
+        }
+
         return res;
     }
 }
