7de18e9017423e3d3b112d15b60229170ea663ac
Suraj Naik <suraj.b.naik@live.com>
2023-11-28 12:54:08 +0530

    Recover BST

--- JAVA/DSA/src/Heaps/RunningMedian.java
+++ JAVA/DSA/src/Heaps/RunningMedian.java
@@ -1,44 +1,36 @@
 package Heaps;
-
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
+import java.util.Collection;
 import java.util.PriorityQueue;
 
 public class RunningMedian {
     public static ArrayList<Integer> solve(ArrayList<Integer> A) {
 
-        ArrayList<Integer> ans = new ArrayList<>();
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        ArrayList<Integer> output = new ArrayList<>();
 
+        ArrayList<Integer> temp = new ArrayList<>();
+
+
+
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        int mid = 0;
         for (int i = 0; i < A.size(); i++) {
-            minHeap.add(A.get(i));
-        }
 
-        int endIndex, index = 0;
-        for (int i = 0; i < A.size(); i++) {
+            pq.add(A.get(i));
 
-            minHeap.add(A.get(i));
-
-            if(minHeap.size() % 2 == 0) {
-                endIndex = (minHeap.size() / 2) - 1;
+            temp.add(pq.remove());
+            if(temp.size() % 2 == 0) {
+                mid = (temp.size() / 2) - 1;
             }
             else {
-                endIndex = (minHeap.size() / 2);
+                mid = temp.size() / 2;
             }
 
-            index = 0;
-            Iterator iterator = minHeap.iterator();
-            while(index <= endIndex){
-                if(index == endIndex){
-                    ans.add((Integer)iterator.next());
-                }
-                if(iterator.hasNext()){
-                    iterator.next();
-                }
-                index++;
-            }
+            output.add(temp.get(mid));
         }
-        return ans;
+
+        return output;
     }
 }
